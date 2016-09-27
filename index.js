d66834969cd430ea52d2c5249d8cded0f4c816e9
Goutham Reddy <gouthamvreddy@gmail.com>
2016-09-27 12:05:35 -0700

    replace spaces in command string with %20 and respond with data from to openWeatherMap Api

--- index.js
+++ index.js
@@ -1,51 +1,13 @@
 'use strict';
 
-/*
-This function handles a Slack slash command and echoes the details back to the user.
-
-Follow these steps to configure the slash command in Slack:
-
-  1. Navigate to https://<your-team-domain>.slack.com/services/new
-
-  2. Search for and select "Slash Commands".
-
-  3. Enter a name for your command and click "Add Slash Command Integration".
-
-  4. Copy the token string from the integration settings and use it in the next section.
-
-  5. After you complete this blueprint, enter the provided API endpoint URL in the URL field.
-
-
-Follow these steps to encrypt your Slack token for use in this function:
-
-  1. Create a KMS key - http://docs.aws.amazon.com/kms/latest/developerguide/create-keys.html.
-
-  2. Encrypt the token using the AWS CLI.
-     $ aws kms encrypt --key-id alias/<KMS key name> --plaintext "<COMMAND_TOKEN>"
-
-  3. Copy the base-64 encoded, encrypted key (CiphertextBlob) to the kmsEncyptedToken variable.
-
-
-Follow these steps to complete the configuration of your command API endpoint
-
-  1. When completing the blueprint configuration select "Open" for security
-     on the "Configure triggers" page.
-
-  2. Enter a name for your execution role in the "Role name" field.
-     Your function's execution role needs kms:Decrypt permissions. We have
-     pre-selected the "KMS decryption permissions" policy template that will
-     automatically add these permissions.
-
-  3. Update the URL for your Slack slash command with the invocation URL for the
-     created API resource in the prod stage.
-*/
-
 const AWS = require('aws-sdk');
 const qs = require('querystring');
+const request = require('superagent');
+const config = require('./config');
 
-const kmsEncryptedToken = '<kmsEncryptedToken>';
+const kmsEncryptedToken = config.kmsEncryptedToken;
+const weatherAPIKey = config.weatherAPIKey;
 let token;
-
 
 function processEvent(event, callback) {
     const params = qs.parse(event.body);
@@ -58,9 +20,16 @@ function processEvent(event, callback) {
     const user = params.user_name;
     const command = params.command;
     const channel = params.channel_name;
-    const commandText = params.text;
+    const commandText = params.text.replace(/ /g,"%20");
 
-    callback(null, `${user} invoked ${command} in ${channel} with the following text: ${commandText}`);
+    request
+      .get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${commandText}&mode=json&units=imperial&cnt=7&appid=${weatherAPIKey}`)
+      .end(function(err, res){
+          // Do something
+          callback(null, res);
+      });
+
+    //callback(null, `${user} invoked ${command} in ${channel} with the following text: ${commandText}`);
 }
 
 
